9a713babecaebff10b724dc9d4b2c6626dbcdaff
ozlmulg <ozlmulg@gmail.com>
2016-03-28 21:45:11 +0300

    changed table sql file

--- conf/script/tables.sql
+++ conf/script/tables.sql
@@ -1,11 +1,10 @@
     drop table if exists product;
 
     create table product (
-        product_id integer not null auto_increment,
         id integer not null,
-        title varchar(255) not null,
+        title varchar(255) not null UNIQUE,
         brand varchar(255) not null,
         category varchar(255) not null,
-        url varchar(255) not null,
-        primary key (product_id)
+        url varchar(255) not null UNIQUE,
+        primary key (id)
     );
